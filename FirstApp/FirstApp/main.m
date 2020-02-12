83ea9cc58b924ca91059424ebb8f2d194edcb59f
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2020-02-12 09:45:08 -0800

    init

---
+++ FirstApp/FirstApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Cyril Garcia on 2/12/20.
+//  Copyright © 2020 Cyril Garcia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
